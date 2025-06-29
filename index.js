15aa3124662449592823aa0cefd7eba23726b6d5
C-Cyril <pro.catteau@gmail.com>
2025-06-29 17:32:39 +0200

    WIP

--- index.js
+++ index.js
@@ -1,20 +1,6 @@
 const express = require("express");
-//import express from "express";
-//const sequelize = require("./BDD/BDD");
-//import sequelize from "./BDD/BDD.js";
 const sequelize = require("./BDD/BDD");
 const Blague = require("./Modele/blague");
-//import Blague from "./Modele/blague.js";
-
-/*
-//Cela a inséré une ligne (id 1) dans la BDD
-Blague.create({id: 1, question: "Que dit un oignon quand il se cogne ?", reponse: "Aïe"});
-Blague.sync();
-*/
-
-
-//console.log(users.every(user => user instanceof User)); // true
-//console.log('All users:', JSON.stringify(users, null, 2));
 
 const app = express();
 
@@ -31,18 +17,20 @@ app.get('/blagues', async (req, res) => {
   res.send(await Blague.toutesLesBlagues());
 });
 
-//Envoi une blague aléatoire PAS FAIT
+//Envoi une blague aléatoire
 app.get('/blagues/random', async (req, res) => {
 	res.send(await Blague.blagueAleatoire());
 });
 
-//Envoi une blague correspondant à son id PAS FAIT
+//Envoi une blague correspondant à son
 app.get('/blagues/:id', async (req, res) => {
 	res.send(await Blague.blagueParId(req.params.id));
 });
 
 //Lance le serveur API
+/*
 const PORT = 3000;
 app.listen(PORT, () => {
 	console.log('My app is running on URL https://localhost:'+PORT);
 });
+*/
