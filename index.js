02dab36e96485e2735a6b71e5511c99b81b5c437
C-Cyril <pro.catteau@gmail.com>
2025-06-30 12:14:45 +0200

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,8 @@ app.use(express.urlencoded({extended: true}));
 
 const cors = require("cors");
 const corsOptions = {
-  origin:"https://c-cyril.github.io/Projet-de-selection-Front/",
+  //origin:"https://c-cyril.github.io/Projet-de-selection-Front/",
+  origin: "*",
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",};
 app.use(cors(corsOptions));
 
