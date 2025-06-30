8ab5da5888157866135f0c8e0a894767e838f40c
C-Cyril <pro.catteau@gmail.com>
2025-06-30 18:21:54 +0200

    Intégration de Swagger dans mon API

--- Modele/blague.js
+++ Modele/blague.js
@@ -1,6 +1,129 @@
 const Sequelize = require("sequelize");
 const sequelize = require("../BDD/BDD");
 
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     blague:
+ *       type: object
+ *       required:
+ *         - id
+ *         - question
+ *         - reponse
+ *       properties:
+ *         id:
+ *           type: integer
+ *           description: Id de la blague, généré automatiquement.
+ *         question:
+ *           type: string
+ *           description: La question posée pour cette blague.
+ *         reponse:
+ *           type: string
+ *           description: La réponse à la question posée dans cette blague.
+ *         createdAt:
+ *           type: string
+ *           format: date
+ *           description: La date à laquelle la blague à été créée, générée automatiquement.
+ *         updatedAt:
+ *           type: string
+ *           format: date
+ *           description: La date à laquelle la blague à été modifiée, générée automatiquement.
+ *       example:
+ *         id: 2
+ *         question: Que dit un oignon quand il se cogne ?
+ *         reponse: Aïe
+ *         createdAt : 2025-06-30T10:39:15.813Z
+ *         updatedAt : 2025-06-30T10:39:15.813Z
+ *     blagueAAjouter:
+ *       type: object
+ *       required:
+ *         - question
+ *         - reponse
+ *       properties:
+ *         question:
+ *           type: string
+ *           description: La question posée pour cette blague.
+ *         reponse:
+ *           type: string
+ *           description: La réponse à la question posée dans cette blague.
+ *       example:
+ *         question: Que dit un oignon quand il se cogne ?
+ *         reponse: Aïe
+ */
+
+/**
+ * @swagger
+ * tags:
+ *   name: Blagues
+ *   description: API qui gère les blagues
+ * /blagues:
+ *   get:
+ *     summary: Affiche toutes les blagues
+ *     tags: [Blagues]
+ *     responses:
+ *       200:
+ *         description: La liste des blagues au format Json.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/blague'
+ *       500:
+ *         description: Erreur serveur.
+ * /ajouter:
+ *   post:
+ *     summary: Créé une nouvelle blague dans la BDD.
+ *     tags: [Blagues]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/blagueAAjouter'
+ *     responses:
+ *       200:
+ *         description: La blague à été créée.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/blagueAAjouter'
+ *       500:
+ *         description: Erreur serveur.
+ * /blagues/random:
+ *   get:
+ *     summary: Affiche une blague au hasard.
+ *     tags: [Blagues]
+ *     responses:
+ *       200:
+ *         description: La blague au format Json.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/blague'
+ *       500:
+ *         description: Erreur serveur.
+  * /blagues/{id}:
+ *   get:
+ *     summary: Affiche une blague dont l'id correspond à celui passé en paramètre.
+ *     tags: [Blagues]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: integer
+ *         required: true
+ *         description: L'id de la blague.
+ *     responses:
+ *       200:
+ *         description: La blague au format Json.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/blague'
+ *       404:
+ *         description: La blague n'a pas été trouvée.
+ */
+
 //création d'un modele "blagues" pointé par "blague"
 const blague = sequelize.define('blagues', {
     id: {
--- index.js
+++ index.js
@@ -2,13 +2,41 @@ const express = require("express");
 const sequelize = require("./BDD/BDD");
 const Blague = require("./Modele/blague");
 
+//Swagger
+const swaggerJsdoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
+
 const app = express();
 //pour le POST
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-//Autorise mon front-end à utiliser cette API
+//Descriptif de l'API via Swagger
+const options = {
+  definition: {
+    openapi: "3.1.0",
+    info: {
+      title: "API Node js de blagues Carambar",
+      version: "0.9.0",
+      description:
+        "API simple faite avec Node js, Express, Sequelize & SQLite.",
+    },
+    servers: [
+      {
+        url: "https://projet-de-selection-back.onrender.com",
+      },
+    ],
+  },
+  apis: ["./modele/*.js"],
+};
+const specs = swaggerJsdoc(options);
+app.use(
+  "/api-docs",
+  swaggerUi.serve,
+  swaggerUi.setup(specs)
+);
 
+//Autorise mon front-end à utiliser cette API
 const cors = require("cors");
 const corsOptions = {
   //Pour autoriser seulement mon front-end : NE FONCTIONNE PAS
@@ -17,7 +45,6 @@ const corsOptions = {
   origin: "*",
   methods: "GET,HEAD,POST",};
 app.use(cors(corsOptions));
-
 
 //Insère une blague dans la BDD, id automatique
 app.post('/ajouter', (req, res) => {
